f714cbfade3911171b27cf79f8ce4c81ec32c331
Kangnos <57825621+Kangnos@users.noreply.github.com>
2020-03-22 00:03:44 +0900

    Add files via upload
    By using prophet module

---
+++ data.py
@@ -0,0 +1,7 @@
+
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.offline as py
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly, add_changepoints_to_plot
+py.init_notebook_mode()
